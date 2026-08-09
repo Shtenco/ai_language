e72df57c13f4b523ff9511c959d0154e4331d2c1
Evgeniy Koshtenko <144296504+Shtenco@users.noreply.github.com>
2026-08-10 01:29:11 +0500

    R5.7 micro: add rapid concept-state falsification runner

---
+++ benchmarks/r57_micro.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import r57_concept_graph_language as r57
+
+r57.SCREEN_STEPS = 32
+r57.SCREEN_BATCH = 8
+r57.LONG_STEPS = 128
+r57.LONG_BATCH = 8
+r57.MODES = ('A_PLAIN', 'D_LOGIC_CYBER')
+
+if __name__ == '__main__':
+    r57.main()
